71b4727d623decc74846ef6b976dd6be4651b69c
AmanApT <amanasstudent@gmail.com>
2025-02-08 18:28:10 +0530

    APIS, UserGames, Session Maintain

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,19 +3,34 @@ const express = require("express");
 const connectDB = require("./config/database");
 const authRoute = require("./routes/authRoute");
 const gameRoute = require("./routes/gameRoute");
+const userRoute = require("./routes/userRoute");
+const sellerRoute = require("./routes/sellerRoute");
 const userAuth = require("./middlewares/userAuth");
 const sellerAuth = require("./middlewares/sellerAuth");
 const cookieParser = require("cookie-parser");
 const PORT = 5000;
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+// Use the CORS middleware
+app.use(cors(corsOptions));
+
 app.use("/api/auth", authRoute);
 app.use("/api/game", gameRoute);
+app.use("/api/user", userRoute);
+app.use("/api/seller", sellerRoute);
 
 //Just for testing purpose
 app.get("/api/test", userAuth, (req, res) => {
