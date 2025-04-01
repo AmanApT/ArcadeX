bea3659cf20abb979a134be8b2230d59d6842386
AmanApT <amanasstudent@gmail.com>
2025-04-01 20:28:21 +0530

    add port

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const paymentRoute = require("./routes/paymentRoute");
 const userAuth = require("./middlewares/userAuth");
 const sellerAuth = require("./middlewares/sellerAuth");
 const cookieParser = require("cookie-parser");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; 
 const app = express();
 const cors = require("cors");
 
